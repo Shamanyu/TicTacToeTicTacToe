99109f38190b429315c9a4af762466d9e61b665b
shamanyu <shubhamshamanyu@gmail.com>
2017-04-16 19:41:29 +0530

    Implement set_state and get_state functions for MiniBoard and Board

--- Board.py
+++ Board.py
@@ -5,6 +5,8 @@ def Board():
 	def __init__(self, size):
 		self.size = size
 		self.board = [[MiniBoard(size) for column in size] for row in size]
+		self.game_state = None
+		self.allowed_players = ['X', 'O']
 
 	def get_size(self):
 		return self.size
@@ -17,5 +19,59 @@ def Board():
 		mini_board.play_move(player, sub_row, sub_column)
 		return self.game_state()
 
-	def game_state(self):
-		return "Still on"
+	def get_game_state(self):
+		return self.game_state
+
+	def set_game_state(self):
+		if self.get_game_state():
+			pass
+		else:
+			player = None
+			for row in self.get_size():
+				for column in self.get_size():
+					if not player:
+						player = self.get_board()[row][column].get_game_state()
+					elif player and self.get_board()[row][column].get_game_state() == player:
+						continue
+					else:
+						player = None
+						break
+				if player:
+					self.game_state = player
+					return True
+			for column in self.get_size():
+				for row in self.get_size():
+					if not player:
+						player = self.get_board()[row][column].get_game_state()
+					elif player and self.get_board()[row][column].get_game_state() == player:
+						continue
+					else:
+						player = None
+						break
+				if player:
+					self.game_state = player
+					return True
+			for index in self.get_size():
+				if not player:
+					player = self.get_board()[index][index].get_game_state()
+				elif player and self.get_board()[index][index].get_game_state() == player:
+					continue
+				else:
+					player = None
+					break
+			if player:
+				self.game_state = player
+				return True
+			for (row, column) in zip(range(0, self.get_size()), range(self.get_size(), 0)):
+				if not player:
+					player = self.get_board()[row][column].get_game_state()
+				elif player and self.get_board()[row][column].get_game_state() == player:
+					continue
+				else:
+					player = None
+					break
+			if player:
+				self.game_state = player
+				return True
+			return True
+
--- MiniBoard.py
+++ MiniBoard.py
@@ -4,6 +4,7 @@ class MiniBoard():
 		self.size = size
 		self.board = [[None for column in range(size)] for row in range(size)]
 		self.allowed_players = ['X', 'O']
+		self.game_state = None
 
 	def get_size(self):
 		return self.size
@@ -14,7 +15,63 @@ class MiniBoard():
 	def play_move(self, player, row, column):
 		if player in set(allowed_players) and row < self.get_size() and column < self.get_size() and self.get_board()[row][column] == None:
 			self.board[row][column] = player
-			return self.game_state()
+			return "Move successful"
+			self.set_game_state()
+		else:
+			return "Invalid move"
 
-	def game_state(self):
-		return "Still on"
+	def get_game_state(self):
+		return self.game_state
+
+	def set_game_state(self):
+		if self.get_game_state():
+			pass
+		else:
+			player = None
+			for row in self.get_size():
+				for column in self.get_size():
+					if not player:
+						player = self.get_board()[row][column]
+					elif player and self.get_board()[row][column] == player:
+						continue
+					else:
+						player = None
+						break
+				if player:
+					self.game_state = player
+					return True
+			for column in self.get_size():
+				for row in self.get_size():
+					if not player:
+						player = self.get_board()[row][column]
+					elif player and self.get_board()[row][column] == player:
+						continue
+					else:
+						player = None
+						break
+				if player:
+					self.game_state = player
+					return True
+			for index in self.get_size():
+				if not player:
+					player = self.get_board()[index][index]
+				elif player and self.get_board()[index][index] == player:
+					continue
+				else:
+					player = None
+					break
+			if player:
+				self.game_state = player
+				return True
+			for (row, column) in zip(range(0, self.get_size()), range(self.get_size(), 0)):
+				if not player:
+					player = self.get_board()[row][column]
+				elif player and self.get_board()[row][column] == player:
+					continue
+				else:
+					player = None
+					break
+			if player:
+				self.game_state = player
+				return True
+			return True
